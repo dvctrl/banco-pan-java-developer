57276e6f39475f7e6602e21634487b38d95005a0
David Santos Mouzinho <davidmouzinho.contato@gmail.com>
2023-02-19 13:32:17 -0300

    adicionando classe 'livro'

--- 1.primeiro-programa-java/primeiro-programa-intellij/src/br/com/dio/PrimeiroProgrma.java
+++ 1.primeiro-programa-java/primeiro-programa-intellij/src/br/com/dio/PrimeiroProgrma.java
@@ -4,11 +4,50 @@ package br.com.dio;
 import br.com.dio.model.Gato;
 
 public class PrimeiroProgrma {
+
+
     public static void main(String[] args) {
 
         Gato gato = new Gato();
         System.out.println(gato);
 
+        Livro livro = new Livro("a volta dos que não foram",300);
+        System.out.println(livro);
+
+
 
     }
 }
+ class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+     public Livro(String nome, Integer numPaginas) {
+         this.nome = nome;
+         this.numPaginas = numPaginas;
+     }
+
+     public String getNome() {
+         return nome;
+     }
+
+     public void setNome(String nome) {
+         this.nome = nome;
+     }
+
+     public Integer getNumPaginas() {
+         return numPaginas;
+     }
+
+     public void setNumPaginas(Integer numPaginas) {
+         this.numPaginas = numPaginas;
+     }
+
+     @Override
+     public String toString() {
+         return "Livro{" +
+                 "nome='" + nome + '\'' +
+                 ", numPaginas=" + numPaginas +
+                 '}';
+     }
+ }
