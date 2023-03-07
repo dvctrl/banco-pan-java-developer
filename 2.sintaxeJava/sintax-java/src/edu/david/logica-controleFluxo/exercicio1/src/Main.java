e50e2beb5c3aafc450e7e22c775adbe6f3c2205c
David Santos Mouzinho <davidmouzinho.contato@gmail.com>
2023-03-07 14:29:50 -0300

    testes de condicionais

---
+++ 2.sintaxeJava/sintax-java/src/edu/david/logica-controleFluxo/exercicio1/src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+        int i1 = 10;
+        int i2 = 20;
+        float f1 = 4.5f;
+        float f2 = 5.6f;
+        char c1 = 'x';
+        char c2 = 'y';
+        String s1 = "Fulano";
+        String s2 = "Siclano";
+        boolean b1 = true;
+        boolean b2 = false;
+        long l1 = 4567l;
+        long l2 = 4333l;
+        byte y1 = 1;
+        short h1 = 25;
+
+        System.out.println(i1 > i2);
+        System.out.println(i1 <= i2);
+        System.out.println(f1 > f2);
+        System.out.println(f1 < f2);
+        System.out.println(c1 == c2);
+        System.out.println(c1 != c2);
+        System.out.println(s1 == s2);
+        System.out.println(s1 != s2);
+        System.out.println(b1 == b2);
+        System.out.println(b1 != b2);
+        System.out.println(l1 != l2);
+        System.out.println(l1 < l2);
+        System.out.println(l1 > l2);
+
+
+
+
+    }
+}
