41d1f5623ce01fdfceac933e5fdcd0026a32cc08
David Santos Mouzinho <davidmouzinho.contato@gmail.com>
2023-03-18 13:54:18 -0300

    conceito de memoria em java

---
+++ 3.Estrutura-de-dados-java/conceitos-basicos/src/Main.java
@@ -0,0 +1,27 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        int intA = 1;
+        int intB = intA;
+
+        System.out.println("intA: "+intA+"\nintB: "+intB);
+        intA = 2;
+        System.out.println("intA: "+intA+"\nintB: "+intB);
+
+        MeuObj ObjA = new MeuObj(1);
+        MeuObj ObjB = ObjA;
+
+        System.out.println("objA: "+ObjA+"\nObjB: "+ObjB);
+        ObjA.setNum(2);
+        System.out.println("objA: "+ObjA+"\nObjB: "+ObjB);
+
+
+
+    }
+
+
+
+
+
+}
